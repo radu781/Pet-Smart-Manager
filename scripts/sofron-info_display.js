6e891cd26ba64cd03b0b86422562d1cce8a2715d
radusofron <radu.sofron169@gmail.com>
2022-06-12 23:44:10 +0300

    Logout button added everywhere; new <p> for my pets

--- scripts/sofron-info_display.js
+++
@@ -1,7 +0,0 @@
-var petCard = document.querySelectorAll(".pet");
-var deleteImg = document.querySelectorAll(".delete");
-
-petCard.addEventListener('mouseover', () => {
-   for(var index = 0; index < deleteImg.length; index++)
-        deleteImg[index].classList.toggle('display');
-})
