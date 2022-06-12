ac742849ee25b3d4e15e9ed06874f0c10e508135
radusofron <radu.sofron169@gmail.com>
2022-06-12 20:04:26 +0300

    Final updates frontend

--- scripts/sofron-info_display.js
+++ scripts/sofron-info_display.js
@@ -2,7 +2,6 @@ const infoButton = document.getElementById("info");
 var infoPanel = document.querySelectorAll(".informations");
 
 infoButton.addEventListener('click', () => {
-    for(let index = 0; index < infoPanel.length; index++)
-        infoPanel[index].classList.toggle('display');
-    infoButton.classList.toggle('accessed');
+   for(var index = 0; index < infoPanel.length; index++)
+    infoPanel[index].classList.toggle('display');
 })
