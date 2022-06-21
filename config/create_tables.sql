db835a3fde92cfa2072cb833aa419d5d8e9269fd
haziflorinmarian <haziflorinmarian@gmail.com>
2022-06-21 10:20:33 +0300

    Updated create_table.sql

--- config/create_tables.sql
+++ config/create_tables.sql
@@ -1,49 +1,116 @@
-CREATE TABLE `group_members` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `group_id` int NOT NULL,
-    `user_id` int NOT NULL,
-    `flags` int DEFAULT NULL,
-    PRIMARY KEY (`id`)
-);
-CREATE TABLE `groups` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `name` varchar(255) NOT NULL,
-    PRIMARY KEY (`id`)
-);
-CREATE TABLE `owned_pets` (
-    `id` int unsigned NOT NULL AUTO_INCREMENT,
-    `pet_id` int NOT NULL,
-    `user_id` int NOT NULL,
-    PRIMARY KEY (`id`)
-);
-CREATE TABLE `pet_info` (
-    `id` int NOT NULL,
-    `name` varchar(255) NOT NULL,
-    `restrictions` varchar(255) DEFAULT NULL,
-    `medical_history` varchar(255) DEFAULT NULL,
-    `relationships` varchar(255) DEFAULT NULL,
-    `breed` varchar(255) DEFAULT NULL,
-    PRIMARY KEY (`id`)
-);
-CREATE TABLE `pet_meals` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `pet_id` int NOT NULL,
-    `feed_time` time NOT NULL,
-    PRIMARY KEY (`id`)
-);
-CREATE TABLE `pet_media` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `pet_id` int NOT NULL,
-    `filename` varchar(255) NOT NULL,
-    `description` varchar(255) DEFAULT NULL,
-    PRIMARY KEY (`id`)
-);
-CREATE TABLE `users` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `email` varchar(255) NOT NULL,
-    `password` varchar(255) NOT NULL,
-    `firstname` varchar(255) NOT NULL,
-    `middlename` varchar(255) DEFAULT NULL,
-    `lastname` varchar(255) NOT NULL,
-    PRIMARY KEY (`id`)
-);
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 13.40.111.219
+ Source Server Type    : MariaDB
+ Source Server Version : 100607
+ Source Host           : 13.40.111.219:3306
+ Source Schema         : pem
+
+ Target Server Type    : MariaDB
+ Target Server Version : 100607
+ File Encoding         : 65001
+
+ Date: 21/06/2022 10:19:25
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for group_members
+-- ----------------------------
+DROP TABLE IF EXISTS `group_members`;
+CREATE TABLE `group_members`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `group_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for group_pet
+-- ----------------------------
+DROP TABLE IF EXISTS `group_pet`;
+CREATE TABLE `group_pet`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `group_id` int(11) NOT NULL,
+  `pet_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 24 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for groups
+-- ----------------------------
+DROP TABLE IF EXISTS `groups`;
+CREATE TABLE `groups`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `owner_id` int(11) NOT NULL,
+  `name` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `invite_hash` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for owned_pets
+-- ----------------------------
+DROP TABLE IF EXISTS `owned_pets`;
+CREATE TABLE `owned_pets`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `pet_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 27 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for pet_info
+-- ----------------------------
+DROP TABLE IF EXISTS `pet_info`;
+CREATE TABLE `pet_info`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `breed` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `restrictions` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `medical_history` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `relationships` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 27 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for pet_meals
+-- ----------------------------
+DROP TABLE IF EXISTS `pet_meals`;
+CREATE TABLE `pet_meals`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pet_id` int(11) NOT NULL,
+  `feed_time` time(0) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 30 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for pet_media
+-- ----------------------------
+DROP TABLE IF EXISTS `pet_media`;
+CREATE TABLE `pet_media`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pet_id` int(11) NOT NULL,
+  `filename` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `description` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `firstname` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `middlename` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `lastname` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 65 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
